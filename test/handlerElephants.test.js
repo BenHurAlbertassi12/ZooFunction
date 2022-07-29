9d9f9fe0cd3170d62cc6d7438885da3f8a784bd9
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-29 18:10:14 -0300

    Test dos elefantes feito 87%, buscar 100%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Quantos elefantes tem na parada?', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Qual é a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Qual é o nome dos elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Quando é possivel visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Qual é a media de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Onde os elefantes estão localizados', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+});
