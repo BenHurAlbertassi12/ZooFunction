d29dc863489545a5148e12b87bedd4caf0b09f77
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-30 17:05:29 -0300

    Arrumada a linha 9 faltava o que ia no 'find'

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,4 +1,4 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
@@ -6,11 +6,10 @@ function getSpeciesByIds(...ids) {
   // caso receba nenhum parâmetro, é necessário retornar um array vazio.
   if (ids.length === 0) return [];
   // ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
-  return ids.map ((bicho) => 
-  data.species.find => species.id === bicho)
+  return ids.map((bicho) => data.species.find((raca) => raca.id === bicho));
 }
-  // ao receber mais de um id, retorna um array com as espécies referentes aos ids.
-console.log(bicho);
+// ao receber mais de um id, retorna um array com as espécies referentes aos ids.
+console.log('info do sapinho', getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274'));
 module.exports = getSpeciesByIds;
 
 // fiz este exercicio com a ajuda do video:
