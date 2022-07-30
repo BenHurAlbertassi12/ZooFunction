bca54c4be47b85a869ef0f6a34f945eeb56f6334
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-30 17:12:38 -0300

    Limpeza do codigo, e alguns comentarios

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,13 @@
 // const { species } = require('../data/zoo_data');
+// esta linha brotou quando eu coloquei '.find', mas o lint não deixa passar
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   // caso receba nenhum parâmetro, é necessário retornar um array vazio.
   if (ids.length === 0) return [];
-  // ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
   return ids.map((bicho) => data.species.find((raca) => raca.id === bicho));
 }
-// ao receber mais de um id, retorna um array com as espécies referentes aos ids.
 console.log('info do sapinho', getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274'));
 module.exports = getSpeciesByIds;
 
