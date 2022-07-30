98f0316aa591c7ae8e5a3b60b6672dfadd93e68f
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-30 19:19:10 -0300

    voltou a passar no teste hehe

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,12 +4,16 @@ function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const idadeAnimalOlder = data.species.forEach((racas) => {
     if (racas.name === animal) {
-      return idadeAnimalOlder.residents.some((residents) >= {
-      residents.age >= age);})
+      return idadeAnimalOlder.residents.some((residents) => {
+        if (residents.age >= age) {
+          return idadeAnimalOlder;
+        }
+      });
     }
-//   });
-// }
-console.log(getAnimalsOlderThan());
+  });
+}
+
+// console.log(getAnimalsOlderThan());
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/every#testing_typed_array_elements_using_arrow_functions
 
