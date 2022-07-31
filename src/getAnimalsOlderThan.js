f8f428b3f3937d829b0621d240c2c9f2e9929720
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 14:52:56 -0300

    não sei mais o que fazer

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,18 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const idadeAnimalOlder = data.species.forEach((racas) => {
-    if (racas.name === animal) {
-      return idadeAnimalOlder.residents.every('residents');
-    }
-    function idadeAnimalOlder(age) {
-      if (data.residents.age >= age) {
-        return getAnimalsOlderThan(idadeAnimalOlder);
-      }
-    }
-  });
+  const idadeAnimalOlder = data.species.map((raca) =>
+    raca.name === animal);
+  idadeAnimalOlder.residents.every((residents) =>
+    residents.age >= age);
 }
-
 // console.log(getAnimalsOlderThan());
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/every#testing_typed_array_elements_using_arrow_functions
