71167a6490a05357b801d6f85421887dc1ad2007
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 16:26:48 -0300

    tentando encontrar o comando certo para retornar

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,11 +1,16 @@
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
   // seu código aqui
   if (employeeName.length === 0) return {};
-  return employeeName.map((colaboradores) => 
-  data.employees.find(()=>)
-
+  return employeeName.map((nome) => {
+    data.employees.firstName
+  }));
+  // data.employees.find((obj) => obj.employees.map([nome])));
 }
+// ✓ sem parâmetros, retorna um objeto vazio (3ms)
+// ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
+// ✕ quando provido o último nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (1ms)
 
 module.exports = getEmployeeByName;
