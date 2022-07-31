ced9b618e1892ff7167f54d7426bbc3dffa5719d
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 20:07:03 -0300

    não consigo entender pq não vai

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,12 +1,15 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(primeiroNome, sobreNome, ...employeeName) {
   // seu código aqui
-  if (employeeName.length === 0) return {};
-  return data.employees.find((nickName) =>
-    (nickName.firstName === employeeName || nickName.lastName === employeeName));
+  if (employeeName.length === 0) {
+    return {};
+  }
+  return employees.find((nik) =>
+    nik.firstName === employeeName || nik.lastName === employeeName);
 }
+
 console.log(getEmployeeByName('Wilburn', 'Burl'));
 // ✓ sem parâmetros, retorna um objeto vazio (3ms)
 // ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
