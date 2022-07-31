45c28535ae2c634f2d0ce3e613a545e41d8e10d5
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 18:33:45 -0300

    apenas para salvar, mudei quase nada

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,14 +1,17 @@
-const { employees } = require('../data/zoo_data');
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
   // seu código aqui
   if (employeeName.length === 0) return {};
   // for (let index = 0; index < employeeName.length; index += 1) {
-  if ((employees.find(firstName) === employeeName) || (employees.find(lastName) === employeeName)){
-    return employeeName;
+  if (data.employees.map(firstName) === employeeName) {
+    return getEmployeeByName;
   }
+  if (data.employees.map(lastName) === employeeName) 
+    return getEmployeeByName;
 }
+
 // ✓ sem parâmetros, retorna um objeto vazio (3ms)
 // ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
 // ✕ quando provido o último nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (1ms)
