4e59f4c12c1a6b6bfbe24b349eab1dc38cd5a6e9
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 15:27:58 -0300

    seguindo a mesma logica do speciesbyid

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(...employeeName) {
   // seu código aqui
+  if (employeeName.length === 0) return {};
+  return employeeName.map((colaboradores) => 
+  data.employees.find(()=>)
+
 }
 
 module.exports = getEmployeeByName;
