af9875b27338b9865a38b9e40242e50b83d38857
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 19:22:30 -0300

    fazendo teste em uma linha para ver se vai -.-

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,17 +1,13 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(firstName, lastName, ...employeeName) {
   // seu código aqui
   if (employeeName.length === 0) return {};
   // for (let index = 0; index < employeeName.length; index += 1) {
-  if (data.employees.find(firstName) === employeeName) {
-    getEmployeeByName(firstName, lastName, ...employeeName);
+  if ((employees.find(firstName) === employeeName) || (employees.find(lastName) === employeeName)) {
+    return getEmployeeByName(employeeName);
   }
-  if (data.employees.find(lastName) === employeeName) {
-    return getEmployeeByName(data.employees.find(...));
-  }
-  console.log(getEmployeeByName('Nelson'));
 }
 
 // ✓ sem parâmetros, retorna um objeto vazio (3ms)
