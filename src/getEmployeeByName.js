dedbb92c340ad83689eba285233ae39562136e2b
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 16:44:05 -0300

    de volta a estava 0

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,13 +1,9 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
   // seu código aqui
   if (employeeName.length === 0) return {};
-  return employeeName.map((nome) => {
-    data.employees.firstName
-  }));
-  // data.employees.find((obj) => obj.employees.map([nome])));
 }
 // ✓ sem parâmetros, retorna um objeto vazio (3ms)
 // ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
