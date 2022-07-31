f7f510f02ae5306baa3d5224bfdec154371539df
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 20:30:26 -0300

    Descobri os bugs, esta comentado o que fiz

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,18 +1,20 @@
 const { employees } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-function getEmployeeByName(primeiroNome, sobreNome, ...employeeName) {
+function getEmployeeByName(employeeName) {
   // seu código aqui
-  if (employeeName.length === 0) {
-    return {};
-  }
+  if (employeeName === undefined) return {};
+
   return employees.find((nik) =>
     nik.firstName === employeeName || nik.lastName === employeeName);
 }
 
-console.log(getEmployeeByName('Wilburn', 'Burl'));
-// ✓ sem parâmetros, retorna um objeto vazio (3ms)
-// ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
-// ✕ quando provido o último nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (1ms)
+console.log(getEmployeeByName('Wilburn'));
+console.log(getEmployeeByName('Burl'));
+
+// função dava errada a todo momento pois adcionei no inicio do parametro uma '...'
+// para pegar todos os dados, dava conflito e não sabia como resolver.
+// outro pepino que deu foi alterar o codigo inicial (qua havia dado certo),
+// ao invez de fazer uma comparação com 0 utilizar o undefined
 
 module.exports = getEmployeeByName;
