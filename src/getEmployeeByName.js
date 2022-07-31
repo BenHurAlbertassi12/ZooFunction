e33b08b7dfcff95428519b23d829f81201f56430
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-07-31 19:47:00 -0300

    voltando array vazia igual com if completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,13 +1,13 @@
-const { employees } = require('../data/zoo_data');
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(primeiroNome, sobreNome, employeeName) {
+function getEmployeeByName(primeiroNome, sobreNome, ...employeeName) {
   // seu código aqui
   if (employeeName.length === 0) return {};
   return data.employees.find((nickName) =>
     (nickName.firstName === employeeName || nickName.lastName === employeeName));
 }
-
+console.log(getEmployeeByName('Wilburn', 'Burl'));
 // ✓ sem parâmetros, retorna um objeto vazio (3ms)
 // ✕ quando provido o primeiro nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (3ms)
 // ✕ quando provido o último nome da pessoa colaboradora, retorna o objeto da pessoa colaboradora (1ms)
