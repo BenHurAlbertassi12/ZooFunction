0b81ebfcfb4ed622a0464012354a7ca34a61e4ca
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-01 14:41:48 -0300

    estrutura inicial

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  return data.species.map((animal) => (()=>{}));
 }
+console.log(countAnimals('frogs'));
 
 module.exports = countAnimals;
