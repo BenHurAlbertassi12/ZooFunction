60ff534b5c1e9f18c376ce9e91d9575e3f3d6ff3
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-01 17:05:18 -0300

    requisito getOldestFromFirstSpecies resolvido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
-  return data.species.map((animal) => (()=>{}));
+  if (animal === undefined) {
+    // se o amimal for igual a indefinido, ele vai percorrer o arrei e vai retornar as especies e a quantidade
+    return species.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.name] = currentValue.residents.length;
+      return accumulator;
+    }, {});
+    // reduce retirado da aula 8.4
+    // reduce((accumulator, currentValue, currentIndex, array) => { /* ... */ }, initialValue )
+  }
+
+  if (animal.sex) { // retorna o parametro sex e a quantidade de animais
+    return species
+      .filter((bichu) => bichu.name === animal.specie)[0]
+      .residents.filter((bichu) => bichu.sex === 'female').length;
+  }
+  // pega a especie recebida por parametro e retorna a quantidade de animais
+  return species.filter((bichu) => bichu.name === animal.specie)[0].residents
+    .length;
 }
-console.log(countAnimals('frogs'));
+// seu código aqui
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  if (data.employees.find(id) === id) return {};
 }
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
