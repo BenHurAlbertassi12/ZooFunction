80920d5e0b1465103b76bd5dcaa9e576277e955f
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-01 19:51:18 -0300

    passou um requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,18 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  const gerentes = data.filter(['stephanieId', 'olaId', 'burlId']);
-  let comparacao = gerentes.find((chefia) => chefia === id)
+  // const gerentes = {
+  //   stephanieId: '9e7d4524-363c-416a-8759-8aa7e50c0992',
+  //   olaId: 'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+  //   burlId: '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+  // };
+
+  const gerente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8'];
+  const comparacao = gerente.find((chefia) => chefia === id);
   return data.employees.some((recebeValor) =>
-    recebeValor.id === comparacao && comparacao === id);
+    recebeValor.id === id && id === comparacao);
 }
 
 // retorna fals se o id passado não é de um gerente
