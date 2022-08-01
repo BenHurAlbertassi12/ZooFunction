40f050e22d5430c4bd5e3c6f41779904c7ecda32
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-01 19:27:00 -0300

    mudei o codigo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,24 +2,24 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  // retorna fals se o id passado não é de um gerente
-  const gerente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
-    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
-    'fdb2543b-5662-46a7-badc-93d960fdc0a8'];
-
-  // let valor = true;
-
-  for (let index = 0; index < gerente.length; index += 1) {
-    if (index[0] === id) {
-      return false;
-    }
-    // return = false;
-  }
-  return true;
+  const gerentes = data.filter(['stephanieId', 'olaId', 'burlId']);
+  let comparacao = gerentes.find((chefia) => chefia === id)
+  return data.employees.some((recebeValor) =>
+    recebeValor.id === comparacao && comparacao === id);
 }
 
+// retorna fals se o id passado não é de um gerente
+// const gerente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+//   'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+//   'fdb2543b-5662-46a7-badc-93d960fdc0a8'];
 
+// let valor = true;
 
+// for (let index = 0; index < gerente.length; index += 1) {
+//   if (index[0] === id) {
+//     return false;
+//   }
+// return = false;
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
