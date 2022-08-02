3fc9e4978a15ed14fdef9bbd7fe27e3fc7d37fbe
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-02 15:17:20 -0300

    fazer commit pq ja estava esquecendo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  return entrants.reduce((classe, idade) => {
+    if (idade.age < 18) {
+      classe.child += 1;
+    } else if
+      (idade.age >= 50) {
+      classe.senior += 1;
+    } else
+      (idade.age < 50 && idade.age >= 18) {
+      classe.adult += 1;
+    }
+    return { child: 0, adult: 0, senior: 0 };
+  }
+
+  );
+
 }
 
 function calculateEntry(entrants) {
