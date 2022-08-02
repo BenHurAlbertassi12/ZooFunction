df0138b129c47ac070fbb2ab53f4588128a75af1
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-02 12:00:41 -0300

    apenas para salvar, mexi quase nada

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,36 +1,47 @@
 const data = require('../data/zoo_data');
 
-const gerente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+const gerenteIdNumber = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
   'fdb2543b-5662-46a7-badc-93d960fdc0a8',
   '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+
+// const valor = true;
 
 function isManager(id) {
   // seu código aqui
   // Utilize a função isManager para verificar se a pessoa é gerente ou não e fazer as seguintes verificações
-  const comparacao = gerente.find((chefia) => chefia === id);
+  const comparacao = gerenteIdNumber.find((chefia) => chefia === id);
   return data.employees.some((recebeValor) =>
     recebeValor.id === id && id === comparacao);
 }
-console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
   // Caso a pessoa seja gerente, retorne um array contendo nome e sobrenome das pessoas colaboradoras gerenciadas por essa pessoa
-  if (isManager === true) {
-    const funcionario = data.employees.filter((funcionarios) =>
-      funcionarios.date.managers.push(managerId));
+  const verFal = isManager(managerId);
+  if (verFal === true) {
+    const novoArray = data.employees.filter((funcionarios) =>
+      funcionarios.date.managers.includes(managerId));
     return funcionario.map((funcionarios) =>
       `${funcionarios.firstName} ${funcionarios.lastName}`);
   }
+  console.log(isManager !== true);
   // Caso a pessoa não seja gerente, dispare um erro com a mensagem: 'O id inserido não é de uma pessoa colaboradora gerente!'
 
-  if (isManager(managerId) !== true) {
+  if (isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 }
 console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-// https://www.w3schools.com/jsref/jsref_push.asp
+// https://www.w3schools.com/jsref/jsref_includes_array.asp
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// fruits.push("Kiwi", "Lemon");
+// fruits.includes("Mango");
 
 module.exports = { isManager, getRelatedEmployees };
+
+//  if (isManager === true) {
+//   const funcionario = data.employees.filter((funcionarios) =>
+//   funcionarios.date.managers.includes(managerId));
+// return funcionario.map((funcionarios) =>
+//   `${funcionarios.firstName} ${funcionarios.lastName}`);//
+// }
