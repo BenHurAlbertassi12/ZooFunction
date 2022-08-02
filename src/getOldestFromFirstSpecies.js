7c342b119f308d8d9c5631ff935cb871fbae3c3e
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-02 19:16:58 -0300

    ja nem sei mais o que colocar

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,9 +2,13 @@ const data = require('../data/zoo_data');
 // const novaArray = [];
 
 function getOldestFromFirstSpecies(id) {
-  const funcionario = data.employees.find((ctps) => ctps.id === id);
-  const animais = funcionario.find(responsibleFor({}));
-  return animais { };
+  const funcionario = data.employees.find((ctps) => ctps.id === id).responsibleFor[0];
+  const animais = data.species.filter((especie) => especie.id === funcionario);
+  const idadeMaior = animais.residents.reduceRight((previousValue, currentValue) => {
+    if (currentValue.age > previousValue.age) return previousValue;
+    return currentValue;
+  });
+  return [`${idadeMaior.name} ${idadeMaior.sex} ${idadeMaior.age}`];
 }
 // console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
