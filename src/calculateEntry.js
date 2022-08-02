4087e9977fda70a43d698270eeb145074e12a6e8
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-02 17:21:57 -0300

    CalculateEntry requisito final ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,20 +1,27 @@
-const data = require("../data/zoo_data");
+// const { prices } = require("../data/zoo_data");
+// const data = require("../data/zoo_data");
 
 function countEntrants(entrants) {
   // seu código aqui
-  return entrants.reduce((categoria, idade) => {
-    if (idade.age < 18) {
-      categoria.child += 1;
-    } else if (idade.age < 50 && idade.age >= 18) {
-      categoria.adult += 1;
-    } else (idade.age > 50)
-    categoria.senior += 1;
-  }); // chave do retorno
-  { child: 0, adult: 0, senior: 0 }
-} // chaves inicial
+  const child = entrants.filter((pessoinha) => pessoinha.age < 18).length;
+  const adult = entrants.filter((pessoinha) => pessoinha.age >= 18 && pessoinha.age < 50).length;
+  const senior = entrants.filter((pessoinha) => pessoinha.age >= 50).length;
 
+  return { child, adult, senior };
+}
+const precoCrianca = 20.99;
+const precoAdulto = 49.99;
+const precoVeio = 24.99;
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+  const valorTotal = {
+    child: (countEntrants(entrants).child * precoCrianca),
+    adult: (countEntrants(entrants).adult * precoAdulto),
+    senior: (countEntrants(entrants).senior * precoVeio),
+  };
+  return valorTotal.child + valorTotal.adult + valorTotal.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
