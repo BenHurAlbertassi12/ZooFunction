54589528eb48a40abf1edc121d6019fba0190a11
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-02 16:29:41 -0300

    sei la

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,23 +1,17 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function countEntrants(entrants) {
   // seu código aqui
-  return entrants.reduce((classe, idade) => {
+  return entrants.reduce((categoria, idade) => {
     if (idade.age < 18) {
-      classe.child += 1;
-    } else if
-      (idade.age >= 50) {
-      classe.senior += 1;
-    } else
-      (idade.age < 50 && idade.age >= 18)
-    classe.adult += 1;
-  }   
-}
-return classe;
-
-  );
-{ child: 0, adult: 0, senior: 0 }
-}
+      categoria.child += 1;
+    } else if (idade.age < 50 && idade.age >= 18) {
+      categoria.adult += 1;
+    } else (idade.age > 50)
+    categoria.senior += 1;
+  }); // chave do retorno
+  { child: 0, adult: 0, senior: 0 }
+} // chaves inicial
 
 function calculateEntry(entrants) {
   // seu código aqui
