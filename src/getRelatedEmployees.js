98e62a3798eb2d2164f5bf7d948d4cc2c117f299
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-01 22:13:36 -0300

    codigo de gerente colocado errado, que ódio

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 const gerente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
   'fdb2543b-5662-46a7-badc-93d960fdc0a8',
-  'fdb2543b-5662-46a7-badc-93d960fdc0a8'];
+  '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
 
 function isManager(id) {
   // seu código aqui
@@ -11,6 +11,8 @@ function isManager(id) {
   return data.employees.some((recebeValor) =>
     recebeValor.id === id && id === comparacao);
 }
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+
 function getRelatedEmployees(managerId) {
   // seu código aqui
   // Caso a pessoa seja gerente, retorne um array contendo nome e sobrenome das pessoas colaboradoras gerenciadas por essa pessoa
@@ -19,6 +21,11 @@ function getRelatedEmployees(managerId) {
       funcionarios.managers.push((isManager)));
     return funcionario.map((funcionarios) => `${funcionarios.firstName}${funcionarios.lastName}`);
   }
+  // Caso a pessoa não seja gerente, dispare um erro com a mensagem: 'O id inserido não é de uma pessoa colaboradora gerente!'
+
+  // if (isManager(managerId) === false) {
+  //   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  // }
 }
 // https://www.w3schools.com/jsref/jsref_push.asp
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
