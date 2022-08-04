522842fd2f2be23daa52889c009dce0d60bd65e6
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-04 20:15:01 -0300

    inicio da estrutura

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,4 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const horarioExibição = (dia) => species.filter((availability) =>
+  availability.includes(dia)).map(name) => name;
+
+const fechado = (day) => {
+  if (day === 'Monday') {
+    return mondayClose()
+  }
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
