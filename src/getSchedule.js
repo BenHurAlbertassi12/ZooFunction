89a7b86d8b6953879e0e3998aafa6603d11416a2
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-04 20:19:40 -0300

    horario de exibição bugado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,11 +4,18 @@ const data = require('../data/zoo_data');
 const horarioExibição = (dia) => species.filter((availability) =>
   availability.includes(dia)).map(name) => name;
 
+const mondayClose = () => ({
+  Funcionamento: 'closed',
+  Horarios: 'o Zoologico esta Fechado'
+})
+
 const fechado = (day) => {
   if (day === 'Monday') {
     return mondayClose()
   }
 }
+
+
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
