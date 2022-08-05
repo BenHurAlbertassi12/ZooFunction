a4b9e14d25e1b4070864c1056c027e88c5f0ff13
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-05 12:59:50 -0300

    pausa para almoco

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,29 @@
-const { species } = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const horarioExibição = (dia) => species.filter((availability) =>
-  availability.includes(dia)).map(name) => name;
-
-const mondayClose = () => ({
-  Funcionamento: 'closed',
-  Horarios: 'o Zoologico esta Fechado'
+const hExibicao1 = ((dia) => {
+  species.filter('availability');
+  const hExibicao2 = hExibicao1.includes(dia);
+  const hExibicao3 = hExibicao2.map((name) => name);
+  return hExibicao3
 })
 
-const fechado = (day) => {
-  if (day === 'Monday') {
-    return mondayClose()
+const diaFechado = () => ({
+  officeHour: 'closed',
+  exhibition: 'o Zoologico esta Fechado'
+})
+
+const fechado = (dia) => {
+  if (dia === 'Monday') {
+    return diaFechado()
   }
 }
+
+const { open, close } = hours[day];
+return {
+  officeHour: `Open from ${open}am until ${close}pm`,
+  exhibition: animalArrExibition(day),
+};
 
 
 
